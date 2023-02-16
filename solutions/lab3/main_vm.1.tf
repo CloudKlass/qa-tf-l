a7471a13ca4e17439c4babee24ae16909eb24967
CloudKlass <104594551+CloudKlass@users.noreply.github.com>
2023-02-16 11:36:29 +0000

    Instancecountupdate
    Terraform ver. broke instance count

--- solutions/lab3/main_vm.1.tf
+++ solutions/lab3/main_vm.1.tf
@@ -1,12 +1,11 @@
-
 resource "aws_instance" "Web_App" {
   count = var.instance_count
   ami           = "ami-098e42ae54c764c35"
   instance_type = "t3.micro"
   subnet_id = aws_subnet.public_subnet.id
-  #vpc_security_group_ids = ["${aws_security_group.allow_web.id}", "${aws_security_group.allow_ssh.id}"]
+  vpc_security_group_ids = ["${aws_security_group.allow_web.id}", "${aws_security_group.allow_ssh.id}"]
 
-  
+
   tags = {
     Name = "Lab2 EC2 Web App"
     KernelVers = "5.10"
@@ -15,8 +14,9 @@ resource "aws_instance" "Web_App" {
 }
 
 resource "aws_eip" "static_ip_vm" {
+  count = var.instance_count
   vpc      = true
-  instance = aws_instance.Web_App.id
+  instance = "${element(aws_instance.Web_App.*.id, count.index)}"
   }
 
 
