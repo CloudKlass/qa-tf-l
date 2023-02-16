cb24703c0a2998e222807df65db43e070ec4c70c
CloudKlass <104594551+CloudKlass@users.noreply.github.com>
2023-02-16 10:25:46 +0000

    Update main_edit4.tf

--- solutions/lab2/main_edit4.tf
+++ solutions/lab2/main_edit4.tf
@@ -31,14 +31,14 @@ resource "aws_eip" "static_ip" {
   instance = aws_instance.Web_App.id
   }
 
-resource "aws_instance" "SQL_Server" {
-  ami           = "ami-0c6b9e5ead23436e4"
-  instance_type = "t2.large"
+ resource "aws_instance" "SQL_Server" {
+  ami           = "ami-07c12b11bae4091a0" #Note: if 400 error is thrown choose another AMI ID (if AWS update, will no longer give access) - Browse console to find a new AMI *later we will do this dynamically through a lookup
+  instance_type = "t3.xlarge" #Note: Change in AMI may require another supported instance type - Try t3.xlarge as noted here
 
   tags = {
-    Name = "MS SQL Server 2017"
+    Name = "MS SQL Server 2022"
     Edition = "Standard"
-    Windowsver = "2019"
+    Windowsver = "2022"
     AMI_region = "Oregon"
   }
 }
